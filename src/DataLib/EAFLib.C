20ac7bd47ded65cbdd4c1eeea57c49d5a3465b11
Anupama Rajendra <asrajendra@wisc.edu>
2026-03-02 11:38:44 -0600

    Revert "make sure 3 digits are parsed"
    This reverts commit cb5e23a23bf6c4850eb97b45755fafdab6a90af9.

--- src/DataLib/EAFLib.C
+++ src/DataLib/EAFLib.C
@@ -160,9 +160,7 @@ void EAFLib::getTransInfo()
   /* truncate to make room for number of parents */
   transTitle[76] = '\0';
   /* parse number of groups from title card */
-  char num[4] = {0};
-  strncpy(num, gp-3, 3);
-  nGroups = atoi(num);
+  nGroups = atoi(strstr(transTitle,"Gp")-3);
 
   grpBnds    = new float[nGroups+1]();
   grpWeights = new float[nGroups]();
