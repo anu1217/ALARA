c01a2b05b70e68b81d169e62a96f29619a032a28
Anupama Rajendra <asrajendra@wisc.edu>
2026-08-12 15:22:28 -0500

    change verbose output

--- src/alara.C
+++ src/alara.C
@@ -181,8 +181,8 @@ int main(int argc, char *argv[])
   Input problemInput(inFname);
 
   /* INPUT */
-  verbose(-1, "This simulation was run with Git commit: %s", GIT_COMMIT_HASH);
-  verbose(-1, "This simulation was run with a Git commit made on: %s", GIT_COMMIT_DATE);
+  verbose(-1, "The version of ALARA used to run this simulation was built with Git commit: %s", GIT_COMMIT_HASH);
+  verbose(-1, "The version of ALARA used to run this simulation was built with a Git commit made on: %s", GIT_COMMIT_DATE);
   verbose(0,"Starting problem input processing.");
   verbose(1,"Reading input.");
   problemInput.read();
